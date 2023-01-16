a2cdedf5d9f927157553601bf881d77c24b31e23
4javier <4javier4@gmail.com>
2023-01-16 19:36:59 +0100

    Initial commit

---
+++ apps/showroom/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'showroom-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'showroom';
+}
