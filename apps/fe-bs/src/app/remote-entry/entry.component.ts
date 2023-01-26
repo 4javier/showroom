b75534b958bfb6be235a2902b2cb26446257d5c9
4javier <4javier4@gmail.com>
2023-01-26 21:45:34 +0100

    add navlist, container and icons

--- apps/fe-bs/src/app/remote-entry/entry.component.ts
+++ apps/fe-bs/src/app/remote-entry/entry.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, Renderer2, ViewEncapsulation } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { NavListComponent } from './nav-list/nav-list.component';
 import { RouterOutlet } from '@angular/router';
 
@@ -11,4 +11,23 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['remote-entry.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  constructor(
+    @Inject(DOCUMENT) private readonly document: Document,
+    private renderer: Renderer2) {
+      const iconLink =  renderer.createElement('link');
+
+      renderer.setAttribute(
+        iconLink, 
+        'href', 
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css'
+      );
+      renderer.setAttribute(
+        iconLink, 
+        'rel',
+        'stylesheet'
+      );
+
+      renderer.appendChild(document.head, iconLink);
+  }
+}
