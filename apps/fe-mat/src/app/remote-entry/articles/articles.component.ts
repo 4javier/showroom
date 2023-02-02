47f750894ad73cf64933f9712afcdfe1da1b9f28
4javier <4javier4@gmail.com>
2023-02-02 19:23:24 +0100

    add carousel to articles page

--- apps/fe-mat/src/app/remote-entry/articles/articles.component.ts
+++ apps/fe-mat/src/app/remote-entry/articles/articles.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticlesFetchingService } from '@showroom/shared/data-fetching';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'showroom-articles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CarouselComponent],
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent {}
+export class ArticlesComponent {
+
+  articleSlides;
+  constructor(private fetch: ArticlesFetchingService) {
+
+    this.articleSlides = fetch.devtoArticlesSlide$;
+  }
+}
