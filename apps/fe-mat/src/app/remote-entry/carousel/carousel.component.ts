9af480aef7f3775f9384b2f246554f789280131a
4javier <4javier4@gmail.com>
2023-02-02 19:22:49 +0100

    add working carousel to material

--- apps/fe-mat/src/app/remote-entry/carousel/carousel.component.ts
+++ apps/fe-mat/src/app/remote-entry/carousel/carousel.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card'
 import { SlideData } from 'libs/shared/data-fetching/src/lib/model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'showroom-carousel',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent {
 
-  slides?: SlideData[];
+  @Input()
+  slides: SlideData[] = [];
+  _focusIndex = 0;
+  translation = ''
+  set focusedIndex(i: number) {
+    i === 0 
+      ? this.translation = ''
+      : this.translation = `transform: translateX(-${75+85*(i-1)}%)`
+    this._focusIndex = i;
+  };
+  get focusedIndex() {
+    return this._focusIndex;
+  }
+
+  nextSlide() {
+    this.focusedIndex < this.slides.length-1 ? this.focusedIndex++ : null
+  }
+
+  prevSlide() {
+    this.focusedIndex > 0 ? this.focusedIndex-- : null
+  }
 
 }
