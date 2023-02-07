52a7bad6e21c27a78cc644846ac755b88b17dfd4
4javier <4javier4@gmail.com>
2023-02-07 17:50:42 +0100

    add carousel autoplay feature

--- apps/fe-mat/src/app/remote-entry/carousel/carousel.component.ts
+++ apps/fe-mat/src/app/remote-entry/carousel/carousel.component.ts
@@ -4,7 +4,8 @@ import { MatCardModule } from '@angular/material/card'
 import { SlideData } from 'libs/shared/data-fetching/src/lib/model';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { BehaviorSubject, filter, interval, repeat, take, takeUntil } from 'rxjs';
+import { BehaviorSubject, combineLatest, interval, NEVER, ReplaySubject, scan, startWith, switchMap, takeUntil } from 'rxjs';
+import { log } from 'console';
 
 @Component({
   selector: 'showroom-carousel',
@@ -28,15 +29,22 @@ export class CarouselComponent {
   get focusedIndex() {
     return this._focusIndex;
   }
-  paused$ = new BehaviorSubject<boolean>(false);
+  private paused$ = new BehaviorSubject<boolean>(false);
+  private toggleAutoplay$ = new BehaviorSubject<void>(void 0)
+  private stopped$ = this.toggleAutoplay$.pipe(
+    scan((acc, _) => !acc, true)
+  );
+  private destroy$ = new ReplaySubject<boolean>();
+  
   ngOnInit() {
-    interval(3000).pipe(
-      take(this.slides.length),
-      takeUntil(this.paused$.pipe(filter(value => !!value))),
-      repeat({delay: () => this.paused$.pipe(filter(value => !value))})
+
+    combineLatest([this.paused$, this.stopped$]).pipe(
+      switchMap(([paused, stopped]) => !!paused || !!stopped ? NEVER : interval(3000)),
+      takeUntil(this.destroy$)
     )
-  //  .subscribe(this.cycleSlides)
+    .subscribe(this.cycleSlides)
   }
+
   nextSlide() {
     this.focusedIndex < this.slides.length-1 ? this.focusedIndex++ : null
   }
@@ -50,5 +58,22 @@ export class CarouselComponent {
       this.focusedIndex = 0;
     else 
       this.focusedIndex++;
+  }
+
+  pauseOn() {
+    this.paused$.next(true)
+  }
+
+  pauseOff() {
+    this.paused$.next(false)
+  }
+
+  toggleStop() {
+    this.toggleAutoplay$.next();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
