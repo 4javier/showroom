0c86a884e3a85d778bdc247800d25344a0a31fab
4javier <4javier4@gmail.com>
2023-02-11 13:25:30 +0100

    add carousel to projects

--- apps/fe-bs/src/app/remote-entry/projects/projects.component.ts
+++ apps/fe-bs/src/app/remote-entry/projects/projects.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReposFetchingService } from '@showroom/shared/data-fetching';
+import { BsCarouselComponent } from '../bs-carousel/bs-carousel.component';
 
 @Component({
   selector: 'showroom-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BsCarouselComponent],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+
+  repoSlides;
+  constructor(private fetcher: ReposFetchingService) {
+    
+    this.repoSlides = fetcher.repoSlides$
+  }
+}
