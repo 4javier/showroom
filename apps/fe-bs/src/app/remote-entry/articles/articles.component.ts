e403b98b7fb4001d8b28d1f418e712dfef97aa36
4javier <4javier4@gmail.com>
2023-02-11 13:19:54 +0100

    add carousel to articles

--- apps/fe-bs/src/app/remote-entry/articles/articles.component.ts
+++ apps/fe-bs/src/app/remote-entry/articles/articles.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticlesFetchingService } from '@showroom/shared/data-fetching';
+import { NgbCarousel, NgbCarouselConfig, NgbSlide } from '@ng-bootstrap/ng-bootstrap';
+import { tap } from 'rxjs';
+import { BsCarouselComponent } from '../bs-carousel/bs-carousel.component';
 
 @Component({
   selector: 'showroom-articles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BsCarouselComponent],
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent {}
+export class ArticlesComponent {
+
+  articleSlides;
+  constructor(private fetcher: ArticlesFetchingService) {
+
+    this.articleSlides = fetcher.devtoArticlesSlide$;
+  }
+}
