50884e185be1225d143a11454788233beaafea19
4javier <4javier4@gmail.com>
2023-02-13 12:42:22 +0100

    select needed bs scss modules

--- apps/fe-bs/src/bootstrap.ts
+++ apps/fe-bs/src/bootstrap.ts
@@ -1,13 +1,14 @@
-import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { RemoteEntryComponent } from './app/remote-entry/entry.component';
 import { appRoutes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(RemoteEntryComponent, {
   providers: [
-    importProvidersFrom(
-      RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
-    ),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideAnimations(),
+    provideHttpClient()
   ],
 });
