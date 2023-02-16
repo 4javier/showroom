d7f767e0b2f816c1caa8b2116688a228b24f4d10
4javier <4javier4@gmail.com>
2023-02-16 18:01:57 +0100

    create logo button component

---
+++ apps/shell/src/app/logo-button/logo-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'showroom-logo-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './logo-button.component.html',
+  styleUrls: ['./logo-button.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class LogoButtonComponent {}
