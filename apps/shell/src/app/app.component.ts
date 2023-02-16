1d6285896bf1383b0184b0ee26b61b81b3343dd7
4javier <4javier4@gmail.com>
2023-02-16 18:06:36 +0100

    add logo svg as custom buttons

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
+import { LogoButtonComponent } from './logo-button/logo-button.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, LogoButtonComponent],
   selector: 'showroom-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
