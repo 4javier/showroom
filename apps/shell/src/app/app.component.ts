b0de9c1a1862a141cde661552e01635753574733
4javier <4javier4@gmail.com>
2023-02-27 18:54:17 +0100

    add animated routing logic to shell

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,16 +1,33 @@
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, ChildrenOutletContexts, NavigationEnd, NavigationStart, OutletContext, Router, RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 import { LogoButtonComponent } from './logo-button/logo-button.component';
-import { homeAnimationSequence } from './animations';
+import { RoutingAnimationService } from '@showroom/shared/routing-animation';
+import { filter, map, Observable, shareReplay, tap } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, LogoButtonComponent],
+  imports: [RouterModule, AsyncPipe, NgIf, LogoButtonComponent],
   selector: 'showroom-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [homeAnimationSequence]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  animState = '';
+
+  activeRoute$: Observable<{url: string}>;
+
+  constructor(
+    router: Router,
+    private ras: RoutingAnimationService,
+  ) {
+    this.activeRoute$ = router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+      map(e => ({url: e.url.split('/')[1]})),
+      shareReplay()
+    )
+  }
+
+  animateRouting(path: string) {
+    this.ras.animateRouting(path)
+  }
 }
