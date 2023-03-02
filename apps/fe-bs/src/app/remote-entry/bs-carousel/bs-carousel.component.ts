396e6ffec72e959eff1e20226ed8f98693b85607
4javier <4javier4@gmail.com>
2023-03-02 20:03:57 +0100

    setup bs placeholder for carousel

--- apps/fe-bs/src/app/remote-entry/bs-carousel/bs-carousel.component.ts
+++ apps/fe-bs/src/app/remote-entry/bs-carousel/bs-carousel.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbCarousel, NgbCarouselConfig, NgbSlide } from '@ng-bootstrap/ng-bootstrap';
 import { SlideData } from 'libs/shared/data-fetching/src/lib/model';
+import { fadeIn } from '../../animations';
 
 @Component({
   selector: 'showroom-bs-carousel',
@@ -9,9 +10,12 @@ import { SlideData } from 'libs/shared/data-fetching/src/lib/model';
   imports: [CommonModule, NgbCarousel, NgbSlide],
   templateUrl: './bs-carousel.component.html',
   styleUrls: ['./bs-carousel.component.scss'],
+  animations: [fadeIn]
 })
 export class BsCarouselComponent {
-
+  
+  @Input()
+  isLoading = false;
   @Input()
   slides: SlideData[] = [];
 
