68a8424cb9b714c182f0e80286fc451932e66023
4javier <4javier4@gmail.com>
2023-03-04 18:48:56 +0100

    add offcanvas for mobile

--- apps/fe-bs/src/app/remote-entry/entry.component.ts
+++ apps/fe-bs/src/app/remote-entry/entry.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavListComponent } from './nav-list/nav-list.component';
 import { RouterOutlet } from '@angular/router';
 import { ShadowRoutingAnimationDirective, shadowSlideLeftAnimation } from '@showroom/shared/shadow-routing-animation'
 import { LightRoutingAnimationHostDirective, lightSlideLeftAnimation } from '@showroom/shared/light-routing-animation'
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   standalone: true,
@@ -20,5 +21,19 @@ import { LightRoutingAnimationHostDirective, lightSlideLeftAnimation } from '@sh
   host: { 'style': 'display: block' }
 })
 export class RemoteEntryComponent {
+  
+  @ViewChild('mainRow') mainRow!: ElementRef<HTMLDivElement>;
+  constructor(
+    private hostElement: ElementRef,
+    private offcanvasService: NgbOffcanvas) {}
+
+	openSidenav() {
+		this.offcanvasService.open(NavListComponent,
+       {
+        container: this.mainRow.nativeElement,
+        scroll: true,
+        panelClass: 'sr-offcanvas-panel'
+      });
+	}
 
 }
