ad9cb87a1d6e22042aabd1233ea78095bd02fbbe
4javier <4javier4@gmail.com>
2023-03-24 00:25:53 +0100

    simplify button show logic

--- apps/fe-bs/src/app/remote-entry/entry.component.ts
+++ apps/fe-bs/src/app/remote-entry/entry.component.ts
@@ -1,20 +1,45 @@
-import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+/* eslint-disable @angular-eslint/no-host-metadata-property */
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavListComponent } from './nav-list/nav-list.component';
 import { RouterOutlet } from '@angular/router';
-import { ShadowRoutingAnimationDirective, shadowSlideLeftAnimation } from '@showroom/shared/shadow-routing-animation'
-import { LightRoutingAnimationHostDirective, LightRoutingAnimationService, lightSlideLeftAnimation } from '@showroom/shared/light-routing-animation'
+import {
+  ShadowRoutingAnimationDirective,
+  shadowSlideLeftAnimation,
+} from '@showroom/shared/shadow-routing-animation';
+import {
+  LightRoutingAnimationHostDirective,
+  LightRoutingAnimationService,
+  lightSlideLeftAnimation,
+} from '@showroom/shared/light-routing-animation';
 import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { ocBtnSlideInFromLeft, ocBtnSlideOutFromLeft } from '../animations';
-import { filter, map, merge, startWith, Subject, withLatestFrom } from 'rxjs';
+import {
+  filter,
+  map,
+  ReplaySubject,
+  startWith,
+  Subject,
+  takeUntil,
+  tap,
+  withLatestFrom,
+} from 'rxjs';
 import { AnimationEvent } from '@angular/animations';
 
 @Component({
   standalone: true,
   imports: [
-    CommonModule, RouterOutlet,
-    ShadowRoutingAnimationDirective, LightRoutingAnimationHostDirective,
-    NavListComponent
+    CommonModule,
+    RouterOutlet,
+    ShadowRoutingAnimationDirective,
+    LightRoutingAnimationHostDirective,
+    NavListComponent,
   ],
   selector: 'showroom-fe-bs-entry',
   templateUrl: 'remote-entry.component.html',
@@ -22,51 +47,57 @@ import { AnimationEvent } from '@angular/animations';
   encapsulation: ViewEncapsulation.ShadowDom,
   providers: [LightRoutingAnimationService],
   animations: [
-    shadowSlideLeftAnimation, lightSlideLeftAnimation,
-    ocBtnSlideInFromLeft, ocBtnSlideOutFromLeft
+    shadowSlideLeftAnimation,
+    lightSlideLeftAnimation,
+    ocBtnSlideInFromLeft,
+    ocBtnSlideOutFromLeft,
   ],
-  host: { 'style': 'display: block' }
+  host: { style: 'display: block' },
 })
-export class RemoteEntryComponent {
-  
+export class RemoteEntryComponent implements OnDestroy {
   @ViewChild('container') container!: ElementRef<HTMLDivElement>;
 
-  hideButton$ = new Subject<void>();
-  isButtonShown$ = this.hideButton$.pipe(startWith(true));
+  hideButton$ = new Subject<boolean>();
+  isButtonShown$ = this.hideButton$.pipe(
+    startWith(false),
+    map((v) => !v),
+  );
+
+  destroy$ = new ReplaySubject<void>();
 
   constructor(
     private offcanvasService: NgbOffcanvas,
     private lras: LightRoutingAnimationService
   ) {
-    this.lras.rendered$.pipe(
-      filter(Boolean),
-      withLatestFrom(this.offcanvasService.activeInstance),
-      map(([,offcanvas]) => offcanvas),
-      filter(Boolean),
-    ).subscribe(offcanvas => offcanvas.close())
+    this.lras.rendered$
+      .pipe(
+        filter(Boolean),
+        withLatestFrom(this.offcanvasService.activeInstance),
+        map(([, offcanvas]) => offcanvas),
+        filter(Boolean),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((offcanvas) => offcanvas.close());
   }
 
-	openSidenav() {
-		const offcanvasRef = this.offcanvasService.open(
-      NavListComponent,
-      {
+  openSidenav() {
+    this.offcanvasService
+      .open(NavListComponent, {
         container: this.container.nativeElement,
         scroll: true,
-        panelClass: 'sr-offcanvas-panel'
-      }
-    );
-
-    this.isButtonShown$ = merge(
-      this.hideButton$.pipe(map(() => false)),
-      offcanvasRef.hidden.pipe(map(() => true))
-    )
-
-	}
+        panelClass: 'sr-offcanvas-panel',
+      })
+      .hidden.subscribe(() => this.hideButton$.next(false));
+  }
 
   buttonGone(ev: AnimationEvent) {
-    if(ev.toState === 'void') {
-      this.openSidenav()
+    if (ev.toState === 'void') {
+      this.openSidenav();
     }
   }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
