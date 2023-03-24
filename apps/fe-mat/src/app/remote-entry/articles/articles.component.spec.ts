1578252848a0ab3e26f611f7d86d452f2b3f3de5
4javier <4javier4@gmail.com>
2023-03-24 11:24:53 +0100

    add mat articles spec

--- apps/fe-mat/src/app/remote-entry/articles/articles.component.spec.ts
+++ apps/fe-mat/src/app/remote-entry/articles/articles.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { ArticlesFetchingService } from '@showroom/shared/data-fetching';
+import { ReplaySubject } from 'rxjs';
 
 import { ArticlesComponent } from './articles.component';
+
+const artFetchServMock = { devtoArticlesSlide$: new ReplaySubject() };
 
 describe('ArticlesComponent', () => {
   let component: ArticlesComponent;
@@ -9,6 +15,10 @@ describe('ArticlesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ArticlesComponent],
+      providers: [
+        provideNoopAnimations(),
+        { provide: ArticlesFetchingService, useValue: artFetchServMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ArticlesComponent);
@@ -18,5 +28,17 @@ describe('ArticlesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start showing loading template', () => {
+    expect(fixture.debugElement.queryAll(By.css('#placeholder'))).toHaveLength(1);
+    expect(fixture.debugElement.queryAll(By.css('showroom-mat-carousel'))).toHaveLength(1);
+  });
+
+  it('should show carousel on fetcher emission', () => {
+    artFetchServMock.devtoArticlesSlide$.next(['whatever']);
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('#placeholder'))).toHaveLength(0);
+    expect(fixture.debugElement.queryAll(By.css('showroom-mat-carousel'))).toHaveLength(1);
   });
 });
