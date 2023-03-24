cea0f010d5cee555081a8b657fa45bb791d2ba41
4javier <4javier4@gmail.com>
2023-03-24 12:31:55 +0100

    add mat nav list spec

--- apps/fe-mat/src/app/remote-entry/nav-list/nav-list.component.spec.ts
+++ apps/fe-mat/src/app/remote-entry/nav-list/nav-list.component.spec.ts
@@ -1,6 +1,15 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
 
 import { NavListComponent } from './nav-list.component';
+
+let harness: RouterTestingHarness;
+
+@Component({})
+class StubComponent {}
 
 describe('NavListComponent', () => {
   let component: NavListComponent;
@@ -9,14 +18,44 @@ describe('NavListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavListComponent],
+      providers: [ 
+        provideRouter([
+          {path: '**', component: StubComponent},
+        ]),
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavListComponent);
     component = fixture.componentInstance;
+    harness = await RouterTestingHarness.create();
     fixture.detectChanges();
-  });
+   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should detect home as active route`,async () => {
+    await harness.navigateByUrl('/home');
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('a#Home')).classes['mdc-list-item--activated']).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('a#Articles')).classes['mdc-list-item--activated']).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('a#Projects')).classes['mdc-list-item--activated']).toBeFalsy();
+  });
+
+  it(`should detect projects as active route`,async () => {
+    await harness.navigateByUrl('/projects');
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('a#Home')).classes['mdc-list-item--activated']).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('a#Articles')).classes['mdc-list-item--activated']).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('a#Projects')).classes['mdc-list-item--activated']).toBeTruthy();
+  });
+
+  it(`should detect articles as active route`,async () => {
+    await harness.navigateByUrl('/articles');
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('a#Home')).classes['mdc-list-item--activated']).toBeFalsy();
+    expect(fixture.debugElement.query(By.css('a#Articles')).classes['mdc-list-item--activated']).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('a#Projects')).classes['mdc-list-item--activated']).toBeFalsy();
   });
 });
