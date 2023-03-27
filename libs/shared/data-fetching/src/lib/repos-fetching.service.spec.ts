d409170158914fa3b7d2af9c60cb83d4687c2346
4javier <4javier4@gmail.com>
2023-03-27 18:33:14 +0200

    add repo fetching service spec

--- libs/shared/data-fetching/src/lib/repos-fetching.service.spec.ts
+++ libs/shared/data-fetching/src/lib/repos-fetching.service.spec.ts
@@ -1,16 +1,137 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { fakeAsync, flush, tick } from '@angular/core/testing';
+import { EMPTY, of } from 'rxjs';
 
-import { ReposFetchingService } from './repos-fetching.service';
+import {
+  GH_API,
+  GH_HOST,
+  GH_USER,
+  GL_API,
+  GL_API_VERSION,
+  GL_HOST,
+  GL_USER,
+  ReposFetchingService,
+  REPO_TOPIC,
+} from './repos-fetching.service';
+import { GithubSerializedRepo, GitlabSerializedRepo, SlideData } from './model';
+
+const httpMock = { get: () => of([]) } as unknown as HttpClient;
+const httpSpy = jest.spyOn(httpMock, 'get');
+
+const githubRepoStub1: GithubSerializedRepo = {
+  description: 'gh_desc1',
+  id: '1',
+  name: 'gh_name1',
+};
+const githubRepoStub2: GithubSerializedRepo = {
+  description: 'gh_desc2',
+  id: '2',
+  name: 'gh_name2',
+};
+const gitlabRepoStub1: GitlabSerializedRepo = {
+  description: 'gl_desc1',
+  id: '1',
+  name: 'gl_name1',
+  topics: [`${REPO_TOPIC}`, 'something_else'],
+};
+const gitlabRepoStub2: GitlabSerializedRepo = {
+  description: 'gl_desc2',
+  id: '2',
+  name: 'gl_name2',
+  topics: [`${REPO_TOPIC}`],
+};
+const otGitlabRepoStub1: GitlabSerializedRepo = {
+  description: 'ot_gl_desc1',
+  id: '1',
+  name: 'ot_gl_name1',
+  topics: ['something_else'],
+};
+const ghSlideStub1: SlideData = {
+  title: 'gh_name1',
+  content: 'gh_desc1',
+  image: 'data:image/webp;base64,gh_img1',
+  link: `${GH_HOST}/${GH_USER}/gh_name1`,
+  origin: 'assets/github-badge.svg',
+};
+const ghSlideStub2: SlideData = {
+  title: 'gh_name2',
+  content: 'gh_desc2',
+  image: 'data:image/webp;base64,gh_img2',
+  link: `${GH_HOST}/${GH_USER}/gh_name2`,
+  origin: 'assets/github-badge.svg',
+};
+const glSlideStub1: SlideData = {
+  title: 'gl_name1',
+  content: 'gl_desc1',
+  image: 'data:image/webp;base64,gl_img1',
+  link: `${GL_HOST}/${GL_USER}/gl_name1`,
+  origin: 'assets/gitlab-badge.svg',
+};
+const glSlideStub2: SlideData = {
+  title: 'gl_name2',
+  content: 'gl_desc2',
+  image: 'data:image/webp;base64,gl_img2',
+  link: `${GL_HOST}/${GL_USER}/gl_name2`,
+  origin: 'assets/gitlab-badge.svg',
+};
 
 describe('ReposFetchingService', () => {
   let service: ReposFetchingService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ReposFetchingService);
-  });
-
   it('should be created', () => {
+    service = new ReposFetchingService(httpMock);
     expect(service).toBeTruthy();
   });
+
+  it('should call API with right parameters', () => {
+    httpSpy.mockClear();
+    service = new ReposFetchingService(httpMock);
+    expect(httpSpy).toBeCalledTimes(2);
+    expect(httpSpy).toHaveBeenCalledWith(`${GH_API}/search/repositories`, {
+      params: new HttpParams({
+        fromString: `q=user:${GH_USER} topic:${REPO_TOPIC}`,
+      }),
+    });
+    expect(httpSpy).toHaveBeenCalledWith(
+      `${GL_API}/${GL_API_VERSION}/users/${GL_USER}/projects`
+    );
+  });
+
+  it('should filter by topic, combines and map projects to slides', fakeAsync(() => {
+    httpSpy.mockImplementation((url) => {
+      if (url.includes(`${GH_API}`)) {
+        if (url.includes('/search/repositories'))
+          return of({
+            total_count: 2,
+            items: [githubRepoStub1, githubRepoStub2],
+          });
+        if (url.includes('gh_name1')) return of({ content: 'gh_img1' });
+        if (url.includes('gh_name2')) return of({ content: 'gh_img2' });
+        else {
+          return EMPTY;
+        }
+      } else if (url.includes(`${GL_API}`)) {
+        if (url.includes(`${GL_USER}`))
+          return of([gitlabRepoStub1, gitlabRepoStub2, otGitlabRepoStub1]);
+        if (url.includes('projects/1')) return of({ content: 'gl_img1' });
+        if (url.includes('projects/2')) return of({ content: 'gl_img2' });
+        else {
+          return EMPTY;
+        }
+      } else {
+        return EMPTY;
+      }
+    });
+
+    service = new ReposFetchingService(httpMock);
+    tick();
+    service.repoSlides$.subscribe((returned) =>
+      expect(returned).toStrictEqual([
+        ghSlideStub1,
+        ghSlideStub2,
+        glSlideStub1,
+        glSlideStub2,
+      ])
+    );
+    flush();
+  }));
 });
--- libs/shared/data-fetching/src/lib/repos-fetching.service.ts
+++ libs/shared/data-fetching/src/lib/repos-fetching.service.ts
@@ -1,99 +1,132 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { catchError, combineLatest, concatAll, concatMap, filter, map, Observable, of, retry, shareReplay, throwError, toArray } from 'rxjs';
-import { GithubRepoDTO, GithubSerializedSearch, GitlabRepoDTO, GitlabSerializedRepo, SlideData } from './model';
+import {
+  catchError,
+  combineLatest,
+  concatAll,
+  concatMap,
+  filter,
+  map,
+  Observable,
+  of,
+  retry,
+  shareReplay,
+  throwError,
+  toArray,
+} from 'rxjs';
+import {
+  GithubRepoDTO,
+  GithubSerializedSearch,
+  GitlabRepoDTO,
+  GitlabSerializedRepo,
+  SlideData,
+} from './model';
 
-const REPO_TOPIC = 'ge-showroom'
+export const REPO_TOPIC = 'ge-showroom';
 
-const GH_HOST = 'https://github.com'
-const GH_API = 'https://api.github.com';
-const GH_USER = '4javier';
-const GH_API_VERSION = '2022-11-28';
+export const GH_HOST = 'https://github.com';
+export const GH_API = 'https://api.github.com';
+export const GH_USER = '4javier';
+export const GH_API_VERSION = '2022-11-28';
 
-const GL_HOST = 'https://gitlab.com'
-const GL_API = 'https://gitlab.com/api';
-const GL_USER = 'gianpiero.errigo';
-const GL_API_VERSION = 'v4';
+export const GL_HOST = 'https://gitlab.com';
+export const GL_API = 'https://gitlab.com/api';
+export const GL_USER = 'gianpiero.errigo';
+export const GL_API_VERSION = 'v4';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReposFetchingService {
-
   githubRepos$: Observable<Array<GithubRepoDTO>>;
   gitlabRepos$: Observable<Array<GitlabRepoDTO>>;
 
   repoSlides$;
 
   constructor(private http: HttpClient) {
-
-    this.githubRepos$ = http.get<GithubSerializedSearch>(
-      `${GH_API}/search/repositories`,
-      {
-    //    headers: new HttpHeaders({'X-GitHub-Api-Version': this.GITHUB_API_VERSION, "Accept": "application/vnd.github+json"}),
-        params: new HttpParams({fromString: `q=user:${GH_USER} topic:${REPO_TOPIC}`}),
-      }
-    ).pipe(
-      retry(5),
-      catchError(() => throwError(() => new Error('An error occurred while fetching repos from Github'))),
-      map(search => search.items),
-      concatAll(),
-      map(repo => (
-        {
-          ...repo, 
+    this.githubRepos$ = http
+      .get<GithubSerializedSearch>(`${GH_API}/search/repositories`, {
+        //    headers: new HttpHeaders({'X-GitHub-Api-Version': this.GITHUB_API_VERSION, "Accept": "application/vnd.github+json"}),
+        params: new HttpParams({
+          fromString: `q=user:${GH_USER} topic:${REPO_TOPIC}`,
+        }),
+      })
+      .pipe(
+        retry(5),
+        catchError(() =>
+          throwError(
+            () =>
+              new Error('An error occurred while fetching repos from Github')
+          )
+        ),
+        map((search) => search.items),
+        concatAll(),
+        map((repo) => ({
+          ...repo,
           origin: 'github',
-          imageUrl: `${GH_API}/repos/${GH_USER}/${repo.name}/contents/showroom_cover.webp`
-        }
-      )),
-      toArray(),
-      shareReplay(1)
-    )
+          imageUrl: `${GH_API}/repos/${GH_USER}/${repo.name}/contents/showroom_cover.webp`,
+        })),
+        toArray(),
+        shareReplay(1)
+      );
 
-    this.gitlabRepos$ = http.get<Array<GitlabSerializedRepo>>(
-      `${GL_API}/${GL_API_VERSION}/users/${GL_USER}/projects`
-    ).pipe(
-      retry(5),
-      catchError(() => throwError(() => new Error('An error occurred while fetching repos from Gitlab'))),
-      concatAll(),
-      filter(repo => repo.topics.includes(`${REPO_TOPIC}`)),
-      map(repo => (
-        {
-          ...repo, 
+    this.gitlabRepos$ = http
+      .get<Array<GitlabSerializedRepo>>(
+        `${GL_API}/${GL_API_VERSION}/users/${GL_USER}/projects`
+      )
+      .pipe(
+        retry(5),
+        catchError(() =>
+          throwError(
+            () =>
+              new Error('An error occurred while fetching repos from Gitlab')
+          )
+        ),
+        concatAll(),
+        filter((repo) => repo.topics.includes(`${REPO_TOPIC}`)),
+        map((repo) => ({
+          ...repo,
           origin: 'gitlab',
-          imageUrl: `${GL_API}/${GL_API_VERSION}/projects/${repo.id}/repository/files/showroom_cover%2Ewebp?ref=master`
-        }
-      )),
-      toArray(),
-      shareReplay(1)
-    )
+          imageUrl: `${GL_API}/${GL_API_VERSION}/projects/${repo.id}/repository/files/showroom_cover%2Ewebp?ref=master`,
+        })),
+        toArray(),
+        shareReplay(1)
+      );
 
-    this.repoSlides$ = combineLatest([this.githubRepos$, this.gitlabRepos$]).pipe(
+    this.repoSlides$ = combineLatest([
+      this.githubRepos$,
+      this.gitlabRepos$,
+    ]).pipe(
       map(([githubRepos, gitlabRepos]) => [...githubRepos, ...gitlabRepos]),
       concatAll(),
-      concatMap(repo => mapRepoToSlide(repo)),
-      toArray()      
-    )
+      concatMap((repo) => mapRepoToSlide(repo)),
+      toArray()
+    );
 
-    const mapRepoToSlide = (repo: GithubRepoDTO | GitlabRepoDTO): Observable<SlideData> => {
-      return this.http.get<{content: string}>(repo.imageUrl).pipe(
-        catchError(() => of({content: ''})),
+    const mapRepoToSlide = (
+      repo: GithubRepoDTO | GitlabRepoDTO
+    ): Observable<SlideData> => {
+      return this.http.get<{ content: string }>(repo.imageUrl).pipe(
+        catchError(() => of({ content: '' })),
 
-        map(imageJson => (
-          {
-            title: repo.name,
-            content: repo.description,
-            image: `data:image/webp;base64,${imageJson.content}`,
-            link: repo.origin === 'github' ? `${GH_HOST}/${GH_USER}/${repo.name}` :
-                  repo.origin === 'gitlab' ? `${GL_HOST}/${GL_USER}/${repo.name}` : 
-                  'no_link',
-            origin: repo.origin === 'github' ? 'assets/github-badge.svg' :
-                    repo.origin === 'gitlab' ? 'assets/gitlab-badge.svg' :
-                    'no_logo'
-          }
-        ))
-      )
-    }
+        map((imageJson) => ({
+          title: repo.name,
+          content: repo.description,
+          image: `data:image/webp;base64,${imageJson.content}`,
+          link:
+            repo.origin === 'github'
+              ? `${GH_HOST}/${GH_USER}/${repo.name}`
+              : repo.origin === 'gitlab'
+              ? `${GL_HOST}/${GL_USER}/${repo.name}`
+              : 'no_link',
+          origin:
+            repo.origin === 'github'
+              ? 'assets/github-badge.svg'
+              : repo.origin === 'gitlab'
+              ? 'assets/gitlab-badge.svg'
+              : 'no_logo',
+        }))
+      );
+    };
   }
-
-
 }
