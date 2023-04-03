4d0b08d654d4540b226862e64fce89795d0e37b7
4javier <4javier4@gmail.com>
2023-04-03 15:37:47 +0200

    add features grid to home

--- apps/fe-mat/src/app/remote-entry/home/home.component.ts
+++ apps/fe-mat/src/app/remote-entry/home/home.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { SrMatTileComponent } from '../sr-mat-tile/sr-mat-tile.component';
+import { map, Observable, startWith } from 'rxjs';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'showroom-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatGridListModule, SrMatTileComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor(){}
+  bp$: Observable<{ isSmall: boolean }>;
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    this.bp$ = breakpointObserver.observe('(max-width: 991px)').pipe(
+      map((isSmall) => ({ isSmall: isSmall.matches })),
+      startWith({ isSmall: false })
+    );
+  }
 }
