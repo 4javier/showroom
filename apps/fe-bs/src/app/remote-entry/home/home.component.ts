4d6a68aee31b7a59fd4b34c324d523b2c58e9514
4javier <4javier4@gmail.com>
2023-04-05 16:40:17 +0200

    add content to bs home

--- apps/fe-bs/src/app/remote-entry/home/home.component.ts
+++ apps/fe-bs/src/app/remote-entry/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SrBsTileComponent } from '../sr-bs-tile/sr-bs-tile.component';
 
 @Component({
   selector: 'showroom-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SrBsTileComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
