f00e943f243ac03cff7f1cbd709cd7591b0d9e73
4javier <4javier4@gmail.com>
2023-04-14 21:02:38 +0200

    fix basic shell home test

--- apps/shell/src/app/home/home.component.spec.ts
+++ apps/shell/src/app/home/home.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { Directive } from '@angular/core';
+import {
+  ShadowRoutingAnimationDirective,
+  ShadowRoutingAnimationService,
+} from '@showroom/shared/shadow-routing-animation';
+import { Subject } from 'rxjs';
+import { LightRoutingAnimationService } from '@showroom/shared/light-routing-animation';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+
+const lrasMock = { rendered$: new Subject<boolean>() };
+
+// eslint-disable-next-line @angular-eslint/directive-selector
+@Directive({ selector: '[sr-sra]', standalone: true })
+class sraStubDirective {}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,8 +23,28 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
-    }).compileComponents();
+      imports: [HomeComponent, RouterTestingModule],
+      providers: [
+        provideNoopAnimations(),
+        { provide: ShadowRoutingAnimationService, useValue: null },
+      ],
+    })
+      .overrideComponent(HomeComponent, {
+        remove: {
+          imports: [ShadowRoutingAnimationDirective],
+        },
+        add: {
+          imports: [sraStubDirective],
+        },
+      })
+      .overrideComponent(HomeComponent, {
+        set: {
+          providers: [
+            { provide: LightRoutingAnimationService, useValue: lrasMock },
+          ],
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
